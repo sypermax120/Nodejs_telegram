c10ae9800c9013ea0b4a521bb2cbbb5cedc9e4bf
sypermax120 <49924496+sypermax120@users.noreply.github.com>
2020-03-19 13:56:49 +0200

    Add files via upload

---
+++ serv.js
@@ -0,0 +1,65 @@
+
+// var x = "asd";
+
+// function test(arg){
+//     count = 0
+//     for(i in arg){
+//         count++
+//     }
+//     console.log(count);
+//     // console.log(arg.length);
+//     // alert(arg.length);
+// }
+
+// test(x)
+
+
+
+
+
+// var x = ["asd", "Maks", "Polka", "privet","pobeda"];
+
+// function test1(arg){
+//     for(element in x){
+//         console.log(arg[element])
+//     }
+// }
+
+// test1(x)
+
+
+
+var x = "asd";
+
+function test(arg, arg2=0){
+    if (arg2 !== arg){
+        alert(arg2);
+    }
+    else{
+        console.log('privet');
+    }
+        
+}
+
+var test1 = function(arg){
+    console.log(arg+'sgrdr');
+}
+
+test(x,x)
+test1(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var c= prompt("sgmldmglkem ")
